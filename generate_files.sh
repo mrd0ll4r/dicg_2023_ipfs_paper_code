e4ccf404f7927252ac17fa823e8afcb1c403145e
Leo Balduf <leobalduf@gmail.com>
2023-09-06 15:19:04 +0200

    *: add script to generate random files with predetermined sizes

---
+++ generate_files.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+base_dir="data/files"
+
+mkdir -p "$base_dir"
+
+for i in $(seq 1 3); do
+    echo "Generating files for server $i..."
+    out_dir="$base_dir/server_${i}"
+    mkdir -p "$out_dir"
+
+    # Dirty hack
+    server='$server'
+    file_sizes=$(mlr --csv --headerless-csv-output filter "$server == $i" then cut -f file_size < plotting/csv/file_sizes.csv)
+
+    for file_size in $file_sizes; do
+        dd if=/dev/urandom bs=1 count=$file_size of="$out_dir/tmp" >/dev/null 2>&1
+        hash=$(sha256sum "$out_dir/tmp" | cut -d' ' -f 1)
+        mv "$out_dir/tmp" "$out_dir/$hash"
+    done
+done
